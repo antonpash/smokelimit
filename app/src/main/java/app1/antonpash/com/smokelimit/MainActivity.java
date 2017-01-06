c1984e52f730e7ee68243e0167ac891cfcb7b95b
antonpash <antonpash@gmail.com>
2017-01-06 17:07:08 +0200

    Initial commit

---
+++ app/src/main/java/app1/antonpash/com/smokelimit/MainActivity.java
@@ -0,0 +1,13 @@
+package app1.antonpash.com.smokelimit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
